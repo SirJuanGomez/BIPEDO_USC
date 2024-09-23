9cc0ef3aa4083441d986986203b68581a84390b2
SirJuanGomez <juangomez154879@gmail.com>
2024-09-23 16:31:34 -0500

    gh

--- Animaciones.py
+++ Animaciones.py
@@ -6,8 +6,8 @@ kit = ServoKit(channels=16)
 
 # Valores iniciales, máximos y mínimos para los servos
 inicial = {0: 90, 1: 85, 2: 90, 3: 100, 4: 85, 5: 120, 
-           6: 75,7: 85, 8: 90, 9: 100, 10: 90, 11: 110, 
-           12: 90, 13: 90, 14: 90, 15: 80}
+           6: 75,7: 85, 8: 90, 9: 100, 10: 90, 11: 120, 
+           12: 90, 13: 90, 14: 90, 15: 100}
 
 maximos = {0: 170, 1: 160, 2: 150, 3: 87, 4: 110, 5: 130,
            6: 110, 7: 90, 8: 90, 9: 150, 10: 175, 11: 100,
@@ -18,7 +18,8 @@ minimos = {0: 90, 1: 45, 2: 25, 3: 90, 4: 55, 5: 50,
            12: 75, 13: 60, 14: 65, 15: 80}
 
 maximo_tmp = {13: 85,
-              7:100}
+              7:100,
+              3:110}
 
 def start():
     print("Servos en posicion inicial..")
@@ -41,7 +42,7 @@ def caminata():
     start()
     print("Iniciando caminata...")
     mover_servos([3,11,15], [inicial[3],inicial[11],inicial[15]],[maximos[3],maximos[11],maximos[15]],5)
-    mover_servos([2,4,6,7,10,12,14], [inicial[2],inicial[4],inicial[6],inicial[7],inicial[10],inicial[12],inicial[14]],[maximos[2],maximos[4],maximos[6],maximo_tmp[7],maximos[10],maximos[12],maximos[14]],5)
+    mover_servos([2,3,4,6,7,10,12,14], [inicial[2],inicial[4],inicial[3],inicial[6],inicial[7],inicial[10],inicial[12],inicial[14]],[maximos[2],maximo_tmp[3],maximos[4],maximos[6],maximo_tmp[7],maximos[10],maximos[12],maximos[14]],5)
     print("Parte 1- Hecho...")
     time.sleep(3)
     mover_servos([3,11,15],[maximos[3],maximos[11],inicial[15]],[minimos[3],minimos[11],inicial[15]],3)
